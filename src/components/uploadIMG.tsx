792ebb5679a0c739a9f355d6581dbe064d531525
AniketSudke <aniketsudke20@gmail.com>
2024-11-01 11:55:19 +0530

    V-1 Done

--- src/components/uploadIMG.tsx
+++ src/components/uploadIMG.tsx
@@ -3,20 +3,33 @@
 import React from "react";
 import { UploadButton } from "@/utils/uploadthing";
 const UploadIMG = () => {
+  const [file, setFile] = React.useState<string | null>(null);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <UploadButton
         endpoint="imageUploader"
         onClientUploadComplete={(res) => {
           // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
+          setFile(res[0].url);
         }}
         onUploadError={(error: Error) => {
           // Do something with the error.
           alert(`ERROR! ${error.message}`);
         }}
       />
+      {file && (
+        <div className="flex">
+          <p className="text-2xl mt-4 text-center  ">{file}</p>
+          <button
+            className="ml-4 bg-yellow-500 px-4 py-2 rounded"
+            onClick={() => {
+              navigator.clipboard.writeText(file);
+            }}
+          >
+            Copy
+          </button>
+        </div>
+      )}
     </main>
   );
 };
